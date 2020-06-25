8b564e796914136ae7144542fac85d6e7b6d6be3
AlvaroKarsz <alvaorka@gmail.com>
2020-06-25 11:25:03 +0300

    added app logo

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from bookList import App
 from tkinter import *
 
 window = Tk()
-window.iconbitmap(default=settings['icons']['trasparent'])
+window.iconbitmap(default=settings['icons']['logo'])
 window.title('My Library')
 App(window,settings,db)
 window.mainloop()
